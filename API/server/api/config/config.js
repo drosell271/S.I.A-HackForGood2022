216deaf9cea351ce12f57d7bea0c4968ddbb920c
Daniel <drmoreno271@gmail.com>
2022-10-22 14:04:07 +0200

    API no

--- API/server/api/config/config.js
+++
@@ -1,5 +0,0 @@
-'use strict'
-const env = require('dotenv').config()
-if (env.error) {
-  throw env.error
-}
--- API/server/api/database/connect.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-// Connect to process.env.MONGODB_URI
-const mongoose = require('mongoose')
-mongoose.connect(process.env.MONGODB_URI, {}).then(() => {
-    console.log('Connected to MongoDB')
-}).catch((e) => {
-    console.log(e)
-})
